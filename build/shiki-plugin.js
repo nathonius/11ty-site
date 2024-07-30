8bf422c80c185c0d41ecc5f01f97014950da6f82
Nathan Smith <nathan@nathan-smith.org>
2024-07-30 00:13:05 -0400

    🔨 refactor: remove useless "eleventy.before" hook in shiki plugin

--- build/shiki-plugin.js
+++ build/shiki-plugin.js
@@ -1,38 +1,33 @@
 import Shiki from "@shikijs/markdown-it";
 
-export function shikiPlugin(config) {
-  // empty call to notify 11ty that we use this feature
-  // eslint-disable-next-line no-empty-function
-  config.amendLibrary("md", () => {});
+const shiki = await Shiki({
+  themes: {
+    light: "vitesse-light",
+    dark: "vitesse-dark",
+  },
+  transformers: [
+    {
+      pre: function () {
+        // Add data-lang="<lang>"
+        this.pre.properties.dataLang = this.options.lang;
 
-  config.on("eleventy.before", async () => {
-    const plugin = await Shiki({
-      themes: {
-        light: "vitesse-light",
-        dark: "vitesse-dark",
+        // Add data-file="<filename>" ; note this is probably unstable
+        const filenameMeta = this.options.meta?.__raw ?? "";
+        if (
+          filenameMeta &&
+          filenameMeta.startsWith("; ") &&
+          filenameMeta.length > 2
+        ) {
+          const filename = filenameMeta.slice(2);
+          this.pre.properties.dataFile = filename;
+        }
       },
-      transformers: [
-        {
-          pre: function () {
-            // Add data-lang="<lang>"
-            this.pre.properties.dataLang = this.options.lang;
+    },
+  ],
+});
 
-            // Add data-file="<filename>" ; note this is probably unstable
-            const filenameMeta = this.options.meta?.__raw ?? "";
-            if (
-              filenameMeta &&
-              filenameMeta.startsWith("; ") &&
-              filenameMeta.length > 2
-            ) {
-              const filename = filenameMeta.slice(2);
-              this.pre.properties.dataFile = filename;
-            }
-          },
-        },
-      ],
-    });
-    config.amendLibrary("md", (mdLib) => {
-      mdLib.use(plugin);
-    });
+export function shikiPlugin(config) {
+  config.amendLibrary("md", (mdLib) => {
+    mdLib.use(shiki);
   });
 }
