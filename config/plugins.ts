b6133a48055656119f71d78bd4e25d7a64ffb103
Nathan Smith <nathan@nathan-smith.org>
2024-12-24 15:18:15 -0500

    🔨 refactor: Remove unused plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,20 +6,16 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import navigation from "@11ty/eleventy-navigation";
 import output from "@11ty/eleventy-plugin-directory-output";
 import RedirectsPlugin from "eleventy-plugin-redirects";
-import emoji from "eleventy-plugin-emoji";
 import type { EleventyConfig } from "../11ty.js";
 import { drafts } from "./drafts.js";
 import { syntaxHighlight } from "./syntax-highlight.js";
 import { registerFilters } from "./filters.js";
-import { registerShortcodes } from "./shortcodes.js";
 import { feed } from "./feed.js";
 import { registerCss } from "./css.js";
 import { registerJsModules } from "./modules.js";
-import { icons } from "./icons.js";
 
 const plugins = [
   { plugin: registerFilters },
-  { plugin: registerShortcodes },
   { plugin: navigation },
   { plugin: IdAttributePlugin },
   {
@@ -36,9 +32,7 @@ const plugins = [
   { plugin: registerJsModules },
   { plugin: drafts },
   { plugin: RedirectsPlugin, options: { template: "netlify" } },
-  { plugin: icons },
   { plugin: feed },
-  { plugin: emoji },
 ];
 
 export default function (config: EleventyConfig) {
